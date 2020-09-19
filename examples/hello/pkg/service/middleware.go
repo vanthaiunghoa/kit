2031e3fb80cfe394ab61fa1297dfea154823fe1f
lilei <lilei@miyaem.com>
2020-09-19 11:50:26 +0800

    gen endpoint mw

--- examples/hello/pkg/service/middleware.go
+++ examples/hello/pkg/service/middleware.go
@@ -1,4 +1,28 @@
 package service
 
-// Middleware describes a service middleware.
+import (
+	"context"
+
+	log "github.com/go-kit/kit/log"
+)
+
 type Middleware func(HelloService) HelloService
+
+type loggingMiddleware struct {
+	logger log.Logger
+	next   HelloService
+}
+
+func LoggingMiddleware(logger log.Logger) Middleware {
+	return func(next HelloService) HelloService {
+		return &loggingMiddleware{logger, next}
+	}
+
+}
+
+func (l loggingMiddleware) Foo(ctx context.Context, s string) (rs string, err error) {
+	defer func() {
+		l.logger.Log("method", "Foo", "s", s, "rs", rs, "err", err)
+	}()
+	return l.next.Foo(ctx, s)
+}
